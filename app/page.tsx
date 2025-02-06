8ae7f0093f9f494cfb56ec790b1b17c69775d3a7
goodfeeling4 <mdshahnebaj649@gmail.com>
2025-02-06 16:46:26 +0000

    ruonded

--- app/page.tsx
+++ app/page.tsx
@@ -6,24 +6,24 @@ import { Toaster } from "@/components/toaster";
 
 export default function Home() {
   return (
-    <main className="relative flex min-h-screen flex-col items-center justify-center">
+    <main className="flex relative flex-col justify-center items-center min-h-screen">
       <Toaster />
       <h1 className="pt-4 pb-8 bg-gradient-to-br from-black via-[#171717] to-[#575757] bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl">
         upload image
       </h1>
-      <div className="bg-white/30 p-12 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto w-full">
+      <div className="p-12 mx-auto w-full max-w-xl rounded-lg ring-1 shadow-xl backdrop-blur-lg bg-white/30 ring-gray-900/5">
         <Uploader />
       </div>
-      <p className="font-light text-gray-600 w-full max-w-lg text-center mt-6">
+      <p className="mt-6 w-full max-w-lg font-light text-center text-gray-600">
         <Link
           href="https://goodfeel4ever.4nets.org/"
-          className="font-medium underline underline-offset-4 p-4 bg-gray-300 hover:text-black transition-colors"
+          className="p-4 font-medium underline bg-gray-300 rounded-md transition-colors underline-offset-4 hover:text-black"
         >
           click to see your impact
         </Link>
         .
       </p>
-      <div className="sm:absolute sm:bottom-0 w-full px-20 py-10 flex justify-between"></div>
+      <div className="flex justify-between px-20 py-10 w-full sm:absolute sm:bottom-0"></div>
     </main>
   );
 }
